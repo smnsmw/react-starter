9e34469187675decf4d11ff82f90a41546238a03
denis.khaidarshin <denis.khaidarshin@arcadia.spb.ru>
2020-10-04 23:55:40 +0300

    wip

--- src/Another.tsx
+++ src/Another.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { FC, useState } from "react";
-
+import styles from './another.module.scss';
 export const Another: FC<any> = () => {
     const [state, setState] = useState(1);
     return (
         <div className={'another'} style={{border: '1px solid gray', display: 'inline-block'}}>
             {state}
-            <button onClick={() => setState((s) => ++s)}>With gay, md</button>
+            <button className={styles.red} onClick={() => setState((s) => ++s)}>With gay, md</button>
         </div>
     )
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,12 @@
 import path from 'path';
 import { HotModuleReplacementPlugin } from 'webpack';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import postcssNormalize from 'postcss-normalize';
+
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
+const isProduction = !isDevelopment;
 const mode = isDevelopment ? 'development' : 'production'
 console.log('Build params', {isDevelopment}); // todo: replace with chalk
 export const config = {
@@ -18,7 +22,9 @@ export const config = {
     },
     module: {
         rules: [
-            {
+          {
+            oneOf: [
+              {
               test: /\.[jt]sx?$/,
               exclude: /node_modules/,
               use: [
@@ -35,11 +41,48 @@ export const config = {
             {
               test: /\.s[ac]ss$/i,
               use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader',
-              ],
+                isDevelopment && 'style-loader',
+                isProduction && MiniCssExtractPlugin.loader,
+                {
+                  loader: 'css-loader',
+                  options: {
+
+                  }
+                },
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    ident: 'postcss',
+                    plugins: [
+                      'postcss-flexbugs-fixes',
+                      'postcss-preset-env',
+                      {
+                        autoprefixer: {
+                          flexbox: 'no-2009',
+                        },
+                        stage: 3,
+                      },
+                      postcssNormalize(),
+                    ],
+                    sourceMap: isProduction,
+                  },
+                },
+                {
+                  loader: 'resolve-url-loader',
+                  options: {
+                    sourceMap: isProduction,
+                  },
+                },
+                {
+                  loader: 'sass-loader',
+                  options: {
+                    sourceMap: true,
+                  }
+                },
+              ].filter(Boolean),
             },
+            ]
+          }
         ]
     },
     plugins: [
@@ -52,7 +95,7 @@ export const config = {
     ].filter(Boolean),
 
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json', '.scss']
     }
 }
 
